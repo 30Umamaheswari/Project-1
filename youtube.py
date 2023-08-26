a6ea2cd0d2f2d7a37aefbbe9211dd68967ddac60
Uma Maheswari <57473981+30Umamaheswari@users.noreply.github.com>
2023-08-26 16:15:47 +0530

    youtube.py

--- youtube.py
+++ youtube.py
@@ -1,22 +1,19 @@
-import json
-
 import mysql
 import pandas as pd
 import streamlit as st
-from bson import ObjectId
 from pymongo import MongoClient
 import mysql.connector
 from googleapiclient.discovery import build
-from bs4 import BeautifulSoup
 from streamlit_option_menu import option_menu
 
-api_key = 'AIzaSyDSdirW9QW42I2R5cJH3ZEn9IsA0SpBZMw'
+api_key = 'AIzaSyBzQfDmoNyB1uiCBTYi0i6Q33VKYzrhyTc'
 youtube_api = build('youtube', 'v3', developerKey=api_key)
 
 video_ids = []
 comment_id = ''
 
 import re
+
 
 def calculate_average_duration(rows):
     channel_durations = {}
@@ -46,9 +43,12 @@ def calculate_average_duration(rows):
     avg_duration_data = []
     for channel_title in channel_durations:
         average_duration_seconds = channel_durations[channel_title] / channel_video_count[channel_title]
-        avg_duration_data.append([channel_title, f"{int(average_duration_seconds // 3600):02}:{int((average_duration_seconds % 3600) // 60):02}:{int(average_duration_seconds % 60):02}"])
+        avg_duration_data.append([channel_title,
+                                  f"{int(average_duration_seconds // 3600):02}:{int((average_duration_seconds % 3600) // 60):02}:{int(average_duration_seconds % 60):02}"])
 
     return avg_duration_data
+
+
 def playlist_datas(c_id):
     pl_data = []
     token = None
@@ -130,7 +130,7 @@ def get_comment_datas(comment_ids):
         except Exception as e:
             comment_info.append({
                 "video_id": comment_ids[i],
-                "comment_id": comment_id,
+                "comment_id": comment['snippet']['topLevelComment']['id'],
                 "comment_text": "Comment is not accessible",
                 "comment_author": "NA",
                 "comment_published_at": "NA"
@@ -163,7 +163,8 @@ def get_video_data(v):
             "video_comments_count": int(video_response["items"][0]["statistics"].get("commentCount", 0)),
             "video_duration": video_response["items"][0]["contentDetails"]["duration"],
             "video_thumbnail": video_response["items"][0]["snippet"]["thumbnails"],
-            "video_caption_status": "Available" if "caption" in video_response["items"][0]["contentDetails"] else "Unavailable",
+            "video_caption_status": "Available" if "caption" in video_response["items"][0][
+                "contentDetails"] else "Unavailable",
             "video_published_at": video_response["items"][0]["snippet"]["publishedAt"],
         }
 
@@ -207,26 +208,41 @@ def channel_details(ch_id):
     v = get_video_data(video_ids)
     com = get_comment_datas(video_ids)
 
-    data = {'channel_info': c,
+    datas = {'channel_info': c,
             'playlist_info': p,
             'video_info': v,
             'comment_info': com}
-    return data
+    return datas
 
 
 def main():
-
-    st.set_page_config(page_title="Youtube Data Retrieving and Storing in different DB",
+    st.set_page_config(page_title="Youtube Data",
                        layout="wide",
-                       initial_sidebar_state="expanded", )
+                       initial_sidebar_state="auto", )
+    page_bg_img = """
+    <style>
+[data-testid="stSidebar"] {
+    background-color: rgba(0, 0, 0, 0.5);  /* Make sidebar background slightly transparent */
+    backdrop-filter: blur(10px);  /* Apply blur effect to the sidebar background */
+}
+
+[data-testid="stAppViewContainer"] {
+    background-image: url("https://images.unsplash.com/photo-1472289065668-ce650ac443d2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2069&q=80");
+    background-size: cover;
+}
+</style>
+    """
+
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    st.title(':blue[Youtube Data Retrieving and Storing in different DB]')
     with st.sidebar:
         selected = option_menu('Harvesting Menu', ["Retrieve & Store", "Data Analysis"],
                                default_index=0,
                                orientation="vertical",
                                styles={"nav-link": {"font-size": "15px", "text-align": "centre", "margin": "0px",
-                                                    "--hover-color": "#aacb88"},
+                                                    "--hover-color": "#98CDDE"},
                                        "container": {"max-width": "6000px"},
-                                       "nav-link-selected": {"background-color": "#aacb88"}})
+                                       "nav-link-selected": {"background-color": "#085973"}})
 
     mydb = mysql.connector.connect(
         host="localhost",
@@ -237,7 +253,7 @@ def main():
     cursor = mydb.cursor()
 
     if selected == "Retrieve & Store":
-        tab1, tab2 = st.tabs(["$\huge RETRIEVE $",  "$\huge STORE $"])
+        tab1, tab2 = st.tabs(["$\huge RETRIEVE $", "$\huge STORE $"])
         with tab1:
             st.write('Enter your Channel Id')
             ch_id = st.text_input('Channel id')
@@ -361,89 +377,123 @@ def main():
                             comment['comment_published_at']
                         )
 
-                        query = "INSERT INTO comment_data (video_id, comment_id, comment_text, comment_author, comment_published_at) " \
-                                "VALUES (%s, %s, %s, %s, %s)"
-                        cursor.execute(query, comment_values)
-                        mydb.commit()
+                        check_query = "SELECT COUNT(*) FROM comment_data WHERE comment_id = %s"
+                        cursor.execute(check_query, (comment['comment_id'],))
+                        count = cursor.fetchone()[0]
+
+                        if count == 0:
+                            # Comment not present, so insert it
+                            query = "INSERT INTO comment_data (video_id, comment_id, comment_text, comment_author, comment_published_at) " \
+                                    "VALUES (%s, %s, %s, %s, %s)"
+                            cursor.execute(query, comment_values)
+                            mydb.commit()
+
+                        # query = "INSERT INTO comment_data (video_id, comment_id, comment_text, comment_author, comment_published_at) " \
+                        #         "VALUES (%s, %s, %s, %s, %s)"
+                        # cursor.execute(query, comment_values)
+                        # mydb.commit()
 
                     # Close the MongoDB connection after all inserts are done
                     client.close()
                     st.write('Data from MongoDB collections successfully stored in MySQL tables')
 
-
     if selected == "Data Analysis":
-        st.write('Choose any one filter')
-        filter_data = st.selectbox('filters', ['To view the names of all the videos and their corresponding channels.',
-                          'To view which channels have the most number of videos, and view how many videos that they have.',
-                          'The top 10 most viewed videos and their respective channels.',
-                          'Total comments were made on each video, and their corresponding video names.',
-                          'To view which videos have the highest number of likes, and their corresponding channel names.',
-                          'The total number of likes and dislikes for each video, and their corresponding video names.',
-                          'The total number of views for each channel, and their corresponding channel names.',
-                          'The names of all the channels that have published videos in the year 2022.',
-                          'The average duration of all videos in each channel, and their corresponding channel names.',
-                          'To view which videos have the highest number of comments, and their corresponding channel names.'])
+        # st.write('Choose any one filter')
+        filter_data = st.selectbox('Choose any one filter', ['To view the names of all the videos and their corresponding channels.',
+                                               'To view which channels have the most number of videos, and view how many videos that they have.',
+                                               'The top 10 most viewed videos and their respective channels.',
+                                               'Total comments were made on each video, and their corresponding video names.',
+                                               'To view which videos have the highest number of likes, and their corresponding channel names.',
+                                               'The total number of likes and dislikes for each video, and their corresponding video names.',
+                                               'The total number of views for each channel, and their corresponding channel names.',
+                                               'The names of all the channels that have published videos in the year 2022.',
+                                               'The average duration of all videos in each channel, and their corresponding channel names.',
+                                               'To view which videos have the highest number of comments, and their corresponding channel names.'])
 
         if filter_data == 'To view the names of all the videos and their corresponding channels.':
-            cursor.execute("SELECT video_data.video_name, channel_data.channel_title FROM video_data JOIN channel_data ON video_data.channel_id = channel_data.channel_id")
+            cursor.execute(
+                "SELECT video_data.video_name, channel_data.channel_title FROM video_data JOIN channel_data ON "
+                "video_data.channel_id = channel_data.channel_id")
             df = pd.DataFrame(cursor.fetchall(), columns=cursor.column_names)
             st.table(df)
 
-        elif filter_data == 'To view which channels have the most number of videos, and view how many videos that they have.':
-            cursor.execute("SELECT channel_data.channel_title, COUNT(video_data.video_id) AS VideoCount FROM channel_data JOIN video_data ON channel_data.channel_id = video_data.channel_id GROUP BY channel_data.channel_title ORDER BY VideoCount DESC;")
+        elif filter_data == ('To view which channels have the most number of videos, and view how many videos that '
+                             'they have.'):
+            cursor.execute(
+                "SELECT channel_data.channel_title, COUNT(video_data.video_id) AS VideoCount FROM channel_data JOIN "
+                "video_data ON channel_data.channel_id = video_data.channel_id GROUP BY channel_data.channel_title "
+                "ORDER BY VideoCount DESC;")
             df = pd.DataFrame(cursor.fetchall(), columns=cursor.column_names)
             st.table(df)
 
         elif filter_data == 'The top 10 most viewed videos and their respective channels.':
-            cursor.execute('SELECT video_data.video_name, channel_data.channel_title, video_data.video_view_count FROM video_data JOIN channel_data ON video_data.channel_id = channel_data.channel_id ORDER BY video_data.video_view_count DESC LIMIT 10')
+            cursor.execute(
+                'SELECT video_data.video_name, channel_data.channel_title, video_data.video_view_count FROM '
+                'video_data JOIN channel_data ON video_data.channel_id = channel_data.channel_id ORDER BY '
+                'video_data.video_view_count DESC LIMIT 10')
             df = pd.DataFrame(cursor.fetchall(), columns=cursor.column_names)
             st.table(df)
 
         elif filter_data == 'Total comments were made on each video, and their corresponding video names.':
-            cursor.execute("SELECT video_data.video_name, COUNT(comment_data.comment_id) AS CommentCount FROM video_data LEFT JOIN comment_data ON video_data.video_id = comment_data.video_id GROUP BY video_data.video_name")
+            cursor.execute(
+                "SELECT video_data.video_name, COUNT(comment_data.comment_id) AS CommentCount FROM video_data LEFT "
+                "JOIN comment_data ON video_data.video_id = comment_data.video_id GROUP BY video_data.video_name")
             df = pd.DataFrame(cursor.fetchall(), columns=cursor.column_names)
             st.table(df)
 
         elif filter_data == 'To view which videos have the highest number of likes, and their corresponding channel names.':
-            cursor.execute("SELECT video_data.video_name, channel_data.channel_title FROM video_data JOIN channel_data ON video_data.channel_id = channel_data.channel_id ORDER BY video_data.video_likes DESC LIMIT 10")
+            cursor.execute(
+                "SELECT video_data.video_name, channel_data.channel_title FROM video_data JOIN channel_data ON "
+                "video_data.channel_id = channel_data.channel_id ORDER BY video_data.video_likes DESC LIMIT 10")
             df = pd.DataFrame(cursor.fetchall(), columns=cursor.column_names)
             st.table(df)
 
         elif filter_data == 'The total number of likes and dislikes for each video, and their corresponding video names.':
-            cursor.execute("SELECT video_data.video_name, SUM(video_data.video_likes) AS TotalLikes, SUM(video_data.video_dislikes) AS TotalDislikes FROM video_data JOIN channel_data ON video_data.channel_id = channel_data.channel_id GROUP BY video_data.video_name")
+            cursor.execute(
+                "SELECT video_data.video_name, SUM(video_data.video_likes) AS TotalLikes, "
+                "SUM(video_data.video_dislikes) AS TotalDislikes FROM video_data JOIN channel_data ON "
+                "video_data.channel_id = channel_data.channel_id GROUP BY video_data.video_name")
             df = pd.DataFrame(cursor.fetchall(), columns=cursor.column_names)
             st.table(df)
 
         elif filter_data == 'The total number of views for each channel, and their corresponding channel names.':
-            cursor.execute("SELECT channel_data.channel_title, SUM(video_data.video_view_count) AS TotalViews FROM channel_data JOIN video_data ON channel_data.channel_id = video_data.channel_id GROUP BY channel_data.channel_title")
+            cursor.execute(
+                "SELECT channel_data.channel_title, SUM(video_data.video_view_count) AS TotalViews FROM channel_data "
+                "JOIN video_data ON channel_data.channel_id = video_data.channel_id GROUP BY "
+                "channel_data.channel_title")
             df = pd.DataFrame(cursor.fetchall(), columns=cursor.column_names)
             st.table(df)
 
         elif filter_data == 'The names of all the channels that have published videos in the year 2022.':
-            cursor.execute("SELECT DISTINCT channel_data.channel_title FROM channel_data JOIN video_data ON channel_data.channel_id = video_data.channel_id WHERE video_data.video_published_at LIKE '2022%'")
+            cursor.execute(
+                "SELECT DISTINCT channel_data.channel_title FROM channel_data JOIN video_data ON "
+                "channel_data.channel_id = video_data.channel_id WHERE video_data.video_published_at LIKE '2022%'")
             df = pd.DataFrame(cursor.fetchall(), columns=cursor.column_names)
             st.table(df)
 
-
         elif filter_data == 'The average duration of all videos in each channel, and their corresponding channel names.':
-            cursor.execute("SELECT channel_data.channel_title, video_data.video_duration FROM channel_data JOIN video_data ON channel_data.channel_id = video_data.channel_id")
+            cursor.execute("SELECT channel_data.channel_title, video_data.video_duration FROM channel_data JOIN "
+                           "video_data ON channel_data.channel_id = video_data.channel_id")
             rows = cursor.fetchall()
             avg_duration_data = calculate_average_duration(rows)
             avg_df = pd.DataFrame(avg_duration_data, columns=["Channel Title", "Average Duration"])
 
             st.table(avg_df)
 
-        elif filter_data == 'To view which videos have the highest number of comments, and their corresponding channel names.':
-            cursor.execute("SELECT video_data.video_name, channel_data.channel_title FROM video_data JOIN channel_data ON video_data.channel_id = channel_data.channel_id ORDER BY video_data.video_comments_count DESC LIMIT 10")
+        elif filter_data == ('To view which videos have the highest number of comments, and their corresponding '
+                             'channel names.'):
+            cursor.execute("SELECT video_data.video_name, channel_data.channel_title FROM video_data JOIN "
+                           "channel_data ON video_data.channel_id = channel_data.channel_id ORDER BY "
+                           "video_data.video_comments_count DESC LIMIT 10")
             df = pd.DataFrame(cursor.fetchall(), columns=cursor.column_names)
             st.table(df)
 
 
-
 # [theme]
 # base="light"
-# backgroundColor="#e8e497"
-# secondaryBackgroundColor="#aacb88"
-# font="serif"
+# primaryColor="#b93c0b"
+# backgroundColor="#d2a685"
+# secondaryBackgroundColor="#c1a176"
+
 if __name__ == '__main__':
     main()
